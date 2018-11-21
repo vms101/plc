c07489162a713d411e995044667006ff36d7a8c4
Victor Martins Soares <vms5@WINDOWS.CIN.UFPE.BR>
2018-11-21 15:56:31 -0200

    aula 4 - youtube corrigida

--- src/slide2/Calculadora.java
+++ src/slide2/Calculadora.java
@@ -3,7 +3,7 @@ package slide2;
 public class Calculadora {
 	private int soma;
 
-	public int somaArray(int [] array) {
+	public synchronized int somaArray(int [] array) {
 		soma = 0;
 
 		for(int i=0; i<array.length; i++) {
